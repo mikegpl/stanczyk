553329a242de05c321ed899388f3b76455d3c311
Michał Grabowski <mkg.grabowski@gmail.com>
2020-12-16 13:21:41 +0100

    Reorganizes repo to reflect structure: proto/ - shared between platforms server/ - server-side app app/ - for android app

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 1. Create and activate venv
 2. Run server_app.py and client_app.py
+
+# stanczyk-app
+todo
--- stanczyk-server/generate_files.sh
+++ stanczyk-server/generate_files.sh
@@ -1,7 +1,7 @@
 python3 -m grpc_tools.protoc \
-        -I proto/ \
+        -I ../proto/ \
         --python_out=./generated \
         --grpc_python_out=./generated \
-        ./proto/*.proto
+        ../proto/*.proto
 
 sed -i -E 's/^import.*_pb2/from . \0/' ./generated/*.py
